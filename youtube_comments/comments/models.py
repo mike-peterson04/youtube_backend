205aaf5304e422279c0c4bcc269cda88d07428f6
Mike Peterson <mpeterson2004@gmail.com>
2021-05-25 12:33:56 -0600

    added comment model

---
+++ youtube_comments/comments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+
+class Comment(models.Model):
+    comment_text = models.CharField(max_length=250)
+    parent = models.ForeignKey('Comment', default=None, blank=True, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(default=datetime.datetime.now)
+
