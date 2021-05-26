e1af5d805a180644f52cf828ff93e8ed6b343852
Mike Peterson <mpeterson2004@gmail.com>
2021-05-26 08:07:16 -0600

    added API end points to make it possible to call the API

--- youtube_comments/comments/urls.py
+++ youtube_comments/comments/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-
+    path('<video_id>/', views.VideoComment.as_view()),
+    path('<video_id>/<int:comment_id>', views.CommentActions.as_view())
 ]
