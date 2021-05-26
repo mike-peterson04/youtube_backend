ab5d94458f14a89566ffdd6cbcbcb226ee54bdae
Mike Peterson <mpeterson2004@gmail.com>
2021-05-26 07:22:04 -0600

    created basic API endpoints for testing

--- youtube_comments/comments/serializer.py
+++ youtube_comments/comments/serializer.py
@@ -5,3 +5,11 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'comment_text', 'parent', 'likes', 'dislikes', 'timestamp', 'video']
+
+    def create_reply(self, parent, validated_data):
+        reply = Comment
+        reply.parent = parent.id
+        reply.comment_text = validated_data.get('comment_text', reply.comment_text)
+        reply.video = validated_data.get('video', reply.video)
+        reply.save()
+        return reply
--- youtube_comments/comments/views.py
+++ youtube_comments/comments/views.py
@@ -3,5 +3,32 @@ from .models import Comment
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializer import CommentSerializer
 # Create your views here.
+
+class VideoComment(APIView):
+    def get(self, request, video_id):
+        comments = Comment.objects.filter(video=video_id)
+        serializer = CommentSerializer(comments,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, video_id):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentActions(APIView):
+
+    # post reply
+    def post(self,request,comment_id):
+
+        parent = Comment.objects.get(pk=comment_id)
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            reply=serializer.create_reply(parent,request.data)
+            return Response(CommentSerializer(reply).data, status=status.HTTP_200_OK)
+
 
